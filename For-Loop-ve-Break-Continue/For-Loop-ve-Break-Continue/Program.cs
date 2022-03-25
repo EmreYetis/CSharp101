4b033614ca353c052a632b183a8452085df86e2e
EmreYetis <emreyetis955@gmail.com>
2022-03-25 21:36:52 +0300

    Kalan Dersler

---
+++ For-Loop-ve-Break-Continue/For-Loop-ve-Break-Continue/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace For_Loop_ve_Break_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekranda girilen sayıya kadar olan tek sayıları yazdır.
+            Console.WriteLine("Bir sayı giriniz:");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            //1 ile 1000 arasındaki tek ve çift sayıların kendi içinde toplanması
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tekToplam += i;
+                }
+                else
+                {
+                    ciftToplam += i;
+                }
+            }
+            Console.WriteLine("Tek Sayıların Toplamı:" + tekToplam);
+            Console.WriteLine("Çift Sayıların Toplamı:" + ciftToplam);
+
+            //Break,Continue
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+
+
+            }
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+
+
+            }
+        }
+    }
+}
---
+++ While_Foreach/While_Foreach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 den başlayarak consoldan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp konsola yazdıralım.
+            Console.Write("Bir Sayı Giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0; ;
+
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            // a dan z ye kadar tüm harfleri konsola yazdır.
+            char harfler = 'a';
+            while (harfler <= 'z')
+            {
+                Console.Write(harfler);
+                harfler++;
+            }
+
+            Console.WriteLine("**** Foreach ****");
+
+            string[] arabalar = { "BMW", "Audi", "Ford", "Renault" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
