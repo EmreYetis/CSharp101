f0090c94880d24c7eb8f121015345933c6fc879b
EmreYetis <emreyetis955@gmail.com>
2022-03-28 15:35:31 +0300

    Telefon_Rehber_Uygulamasi

---
+++ Console Telefon Rehberi Uygulamasi/Console Telefon Rehberi Uygulamasi/Data.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace console_telefon_rehberi_uygulamasi
+{
+    class Contact
+    {
+        private string name;
+        private string surName;
+        private string number;
+        public Contact(string f_name, string l_name, string phone)
+        {
+            this.name = f_name;
+            this.surName = l_name;
+            this.number = phone;
+        }
+
+        public string getFirstName()
+        {
+            return this.name;
+        }
+        public string getLastName()
+        {
+            return this.surName;
+        }
+        public string getPhone()
+        {
+            return this.number;
+        }
+        public void setFirstName(string name)
+        {
+            this.name = name;
+        }
+        public void setLastName(string name)
+        {
+            this.surName = name;
+        }
+        public void setPhone(string phone)
+        {
+            this.number = phone;
+        }
+        public void ContactDetails()
+        {
+            Console.WriteLine("İsim: " + this.name);
+            Console.WriteLine("Soyisim: " + this.surName);
+            Console.WriteLine("Telefon Numarası: " + this.number);
+        }
+
+    }
+
+}
---
+++ Console Telefon Rehberi Uygulamasi/Console Telefon Rehberi Uygulamasi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace console_telefon_rehberi_uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int operation = 0;
+            List<Contact> contactsList = new List<Contact>();
+            ContactsOperations ops = new ContactsOperations();
+            contactsList.Add(new Contact("Emre", "Kaan", "07777777777"));
+            contactsList.Add(new Contact("Batuhan", "Yet", "08888888888"));
+            contactsList.Add(new Contact("Cagatay", "Yetiş", "09999999999"));
+            contactsList.Add(new Contact("Mert", "Aydın", "06666666666"));
+
+            do
+            {
+                ops.ViewOptions();
+                operation = Convert.ToInt16(Console.ReadLine());
+                switch (operation)
+                {
+                    case 1:
+                        ops.AddContact(contactsList);
+                        break;
+                    case 2:
+                        ops.DeleteContact(contactsList);
+                        break;
+                    case 3:
+                        ops.UpdateContact(contactsList);
+                        break;
+                    case 4:
+                        ops.ViewContacts(contactsList);
+                        break;
+                    case 5:
+                        ops.ViewContact(contactsList);
+                        break;
+                }
+            } while (Convert.ToInt16(operation) != 6);
+
+
+        }
+    }
+}
